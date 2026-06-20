152a1b0d4f62dd17ec393ee08dca2394b0e72f77
Heckerman2281337 <maskskozevnikov4@gmail.com>
2026-06-20 22:59:35 +0300

    Added background service for bot(BECAUSE SUDDENLY I FOUND OUT THAT I WAS READING OLD ARTICLES OF API), refactor architecture of program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,4 @@
 ﻿using Telegram.Bot;
-using Telegram.Bot.Types;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using telegramBot.src.Repo;
@@ -13,6 +12,7 @@ namespace telegramBot.src
             string? token = Environment.GetEnvironmentVariable("TOKEN");
             if (token == null) throw new ArgumentException("Токен бота не найден в .env");
 
+            var bot = new TelegramBotClient(token);
             using var cts = new CancellationTokenSource();
             
             // host for Runtime cycle of bot
@@ -20,34 +20,15 @@ namespace telegramBot.src
                 .ConfigureServices(services =>
                 {
                     services.AddDataAcces();
-
                     services.AddScoped<BotHandler>();
+
                     services.AddSingleton<ITelegramBotClient>(new TelegramBotClient(token));
+                    services.AddHostedService<BotBackgroundService>();
                 }
                 )
                 .Build();
 
-            using var scope = host.Services.CreateScope();
-
-            var bot = scope.ServiceProvider.GetRequiredService<ITelegramBotClient>();
-
-            var me = await bot.GetMe();
-            //bot.OnUpdate += OnUpdate;
-
-            var handler = scope.ServiceProvider
-                .GetRequiredService<BotHandler>();
-
-            // Dev debugging
-            Console.WriteLine($"@{me.Username} is running... Press Enter to terminate");
-            Console.ReadLine();
-
-            //await Task.Delay(-1); Prod 
-            /*
-            async Task OnUpdate(Update update)
-            {
-                await handler.HandleUpdateAsync(update, cts.Token);
-            }
-            */
+            await host.RunAsync();
         }
     }
 }
---
+++ src/Services/BotBackgroundService.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace telegramBot.src
+{
+    //Background service for bot so it can shut down safely. Also its much cleaner than making everything in program.cs
+    public class BotBackgroundService : BackgroundService
+    {
+        public BotBackgroundService(ITelegramBotClient client, IServiceProvider serviceProvider) 
+        { 
+            _client = client;
+            _serviceProvider = serviceProvider;
+        }
+
+        private readonly ITelegramBotClient _client;
+        private readonly IServiceProvider _serviceProvider;
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var me = await _client.GetMe(stoppingToken);
+            Console.WriteLine($"@{me.Username} is running... Press Ctrl+C to terminate");
+
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = Array.Empty<UpdateType>() // So telegram can send to bot everything no matter what type of
+                // message is.
+            };
+            //Polling
+            _client.StartReceiving(
+                updateHandler: HandleUpdateAsync,
+                errorHandler: HandlePollingErrorAsync,
+                receiverOptions: receiverOptions,
+                cancellationToken: stoppingToken
+                );
+
+            await Task.Delay(Timeout.Infinite, stoppingToken); // Bot is polling non-stop unless its shuts down
+        }
+
+        
+        private async Task HandleUpdateAsync(ITelegramBotClient client, Update update, CancellationToken cancellationToken)
+        {
+            using var scope = _serviceProvider.CreateScope(); // 1 request - 1 scope
+            var handler = scope.ServiceProvider.GetRequiredService<BotHandler>();
+
+            try
+            {
+                await handler.HandleUpdateAsync(update, cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[Error] Ошибка при обработке обновления: {ex.Message}");
+            }
+        }
+
+        private Task HandlePollingErrorAsync(ITelegramBotClient client, Exception exception, CancellationToken cancellationToken)
+        {
+            Console.WriteLine($"[Telegram API Error]: {exception.Message}");
+            return Task.CompletedTask;
+        }
+    }
+}
+
